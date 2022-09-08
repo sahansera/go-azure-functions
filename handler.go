dff8abff5351331f6f1b4528cfacff6c3f60039a
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2022-09-08 21:26:02 +0930

    Create Go app

---
+++ handler.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+)
+
+// string slice
+var quotes = []string{
+	"Talk is cheap. Show me the code.",
+	"First, solve the problem. Then, write the code.",
+	"Experience is the name everyone gives to their mistakes.",
+	"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+}
+
+func quotesHandler(w http.ResponseWriter, r *http.Request) {
+	// Get a random quote
+	message := quotes[rand.Intn(len(quotes))]
+	fmt.Fprint(w, message)
+}
+
+func main() {
+	listenAddr := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+	}
+	http.HandleFunc("/api/GetQuotes", quotesHandler)
+	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
+}
